aecde3e5fc84bf07baa078919767658276daa13e
skoretskiy <skoretskiy@MBP-C02FD270ML85.exness.local>
2022-08-07 17:51:36 +0300

    test precommit

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return "Hello, World22!"
 
-if __name__ == '__main__':
-	app.run()
+@app.route("/")
+def index():
+    return "Hello, World with pre-commit!"
+
+
+if __name__ == "__main__":
+    app.run()
